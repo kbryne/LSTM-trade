40b5e7d7ea3e41803386ae6fb01291ca78e11f58
kbryne <kristoffer.bryne@gmail.com>
2024-08-01 17:46:35 +0200

    splitted the data into train, test and validation

--- LSTM-trade.py
+++ LSTM-trade.py
@@ -5,11 +5,11 @@ from tensorflow.keras import layers
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 import pandas as pd
+import numpy as np
 
 # fetching data
 stock = yf.Ticker("CRAYN.OL")
 data = stock.history(period="max")
-#print(data.head())
 
 scale_features = ['Open', 'High', 'Low', 'Close', 'Volume']
 no_scale_features = ['Dividends', 'Stock Splits']
@@ -17,12 +17,11 @@ no_scale_features = ['Dividends', 'Stock Splits']
 # scaling the correct features
 sc = MinMaxScaler(feature_range=(0, 1))
 scaled_data = sc.fit_transform(data[scale_features])
-print(scaled_data)
 
 # concatinate the scaled data with the unscaled data
 scaled_df = pd.DataFrame(scaled_data, columns=scale_features, index=data.index)
 final_data = pd.concat([scaled_df, data[no_scale_features]], axis=1)
-print(final_data)
+
 
 # Need to create a sequence of the data
 
@@ -36,6 +35,11 @@ def make_sequence(data, len_secuence=60):
         return np.array(X), np.array(y)
     else:
         return np.array([]), np.array([])
+
+X, y = make_sequence(final_data, len_secuence=60)
+
+X_train, X_test_temp, y_train, y_test_temp = train_test_split(X, y, test_size=0.3, shuffle=False)
+X_val, X_test, y_val, y_test = train_test_split(X_test_temp, y_test_temp, test_size=0.5, shuffle=False)
 
 # initializing lstm
 model = Sequential()
